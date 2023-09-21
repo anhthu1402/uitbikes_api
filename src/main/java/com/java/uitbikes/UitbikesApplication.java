f54b4a80c561e4608dd95756db8e5df83433d2e9
anhthu1402 <20520792@gm.uit.edu,vn>
2023-09-22 00:24:21 +0700

    first commit

---
+++ src/main/java/com/java/uitbikes/UitbikesApplication.java
@@ -0,0 +1,13 @@
+package com.java.uitbikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UitbikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UitbikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/uitbikes/UitbikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.uitbikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UitbikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
